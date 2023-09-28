1d95aa0bb5e78e128883fd35bd99adcc372304e4
John Duncan <johnduncan@Johns-MacBook-Pro.local>
2023-09-27 20:14:14 -0700

    debug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-import App from './App';
+import { createRoot } from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router} from 'react-router-dom';
+import App from './App';
+import { HashRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
-  <Router>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
     <App />
-  </Router>,
-  document.getElementById('root'),
+    </HashRouter>
+  </React.StrictMode>
 );
