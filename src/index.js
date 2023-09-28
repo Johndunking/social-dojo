ae20ec92a2b42becb8f7b223f1245f785c8856f9
John Duncan <johnduncan@Johns-MacBook-Pro.local>
2023-09-27 20:19:53 -0700

    debug

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <HashRouter>
     <App />
     </HashRouter>
-  </React.StrictMode>
 );
