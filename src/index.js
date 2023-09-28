cf859a7702e52d62505f7c273283ae5f47757356
John Duncan <johnduncan@Johns-MacBook-Pro.local>
2023-09-27 19:32:03 -0700

    debug

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
