9c79276411d5da51a61634c5902bbfd1c8324089
John Duncan <johnduncan@Johns-MacBook-Pro.local>
2023-09-27 19:25:02 -0700

    added hashrouter

---
+++ src/components/Login.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+
+import { useNavigate } from 'react-router-dom'
+import {FcGoogle} from 'react-icons/fc'
+import dojoVideo from '../assets/dojoVideo.mp4'
+import Dojo1 from '../assets/Dojo1.png'
+import { GoogleLogin } from '@react-oauth/google';
+
+import { client } from '../client'
+
+import jwt_decode from "jwt-decode";
+
+
+const Login = () => {
+
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject)
+    if (userObject) {
+      localStorage.setItem('user', JSON.stringify(userObject));
+      const doc = {
+            _id: userObject.sub,
+            _type: 'user',
+            userName: userObject.name,
+            image: userObject.picture,
+          };
+          client.createIfNotExists(doc).then(() => {
+            navigate('/', { replace: true });
+          });
+    }
+  };
+
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video src={dojoVideo}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+        autoPlay
+        className="w-full h-full object-cover"
+          />
+          <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0  bg-blackOverlay">
+            <div className="p-5">
+              <img src={Dojo1} width="200px" alt="logo" />
+            </div>
+
+            <div className="shadow-2xl">
+              <GoogleLogin 
+              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+              render={(renderProps) => (
+                <button
+                type="button"
+                className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4 " /> Sign in with Google
+
+                </button>
+                
+
+              )}
+
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+              
+            
+
+              />
+
+            </div>
+
+          </div>
+        
+
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/utils/data.js
@@ -0,0 +1,202 @@
+export const categories = [
+  {
+    name: 'Reading',
+    image: 'https://cdn-icons-png.flaticon.com/512/2232/2232688.png',
+  },
+  {
+    name: 'Math',
+    image: 'https://cdn-icons-png.flaticon.com/512/1011/1011863.png',
+  },
+  {
+    name: 'Writing',
+    image: 'https://cdn-icons-png.flaticon.com/512/4659/4659027.png',
+  },
+  {
+    name: 'Coding',
+    image: 'https://i.pinimg.com/750x/66/b1/29/66b1296d36598122e6a4c5452b5a7149.jpg',
+  },
+  {
+    name: 'SEL',
+    image: 'https://i.pinimg.com/564x/f4/45/c4/f445c4b7eec721298d98777ad0ba31d6.jpg',
+  },
+  {
+    name: 'Recipes',
+    image: 'https://i.pinimg.com/236x/7d/ef/15/7def15ac734837346dac01fad598fc87.jpg',
+  },
+  {
+    name: 'Quotes',
+    image: 'https://i.pinimg.com/236x/46/7c/17/467c17277badb00b638f8ec4da89a358.jpg',
+  },
+  {
+    name: 'Art',
+    image: 'https://i.pinimg.com/736x/f4/e5/ba/f4e5ba22311039662dd253be33bf5f0e.jpg',
+  },
+{}
+];
+
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+  image{
+    asset->{
+      url
+    }
+  },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    } `;
+
+export const pinDetailQuery = (pinId) => {
+  const query = `*[_type == "pin" && _id == '${pinId}']{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    title, 
+    about,
+    category,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+   save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    }
+  }`;
+  return query;
+};
+
+export const pinDetailMorePinQuery = (pin) => {
+  const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const searchQuery = (searchTerm) => {
+  const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+        image{
+          asset->{
+            url
+          }
+        },
+            _id,
+            destination,
+            postedBy->{
+              _id,
+              userName,
+              image
+            },
+            save[]{
+              _key,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+            },
+          }`;
+  return query;
+};
+
+export const userQuery = (userId) => {
+  const query = `*[_type == "user" && _id == '${userId}']`;
+  return query;
+};
+
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const userSavedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
